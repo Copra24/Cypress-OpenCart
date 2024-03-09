d3adc6982c4bc3810e59a7d1c94c5702aca298fc
Copra24 <folefac24.c@gmail.com>
2024-03-09 20:32:35 +0100

    Firt commit setup

---
+++ cypress/e2e/Login_Page/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+describe('Validating login functionality', ()=>{
+    beforeEach(() => {
+        cy.visit('https://demo.opencart.com/admin/')
+      })
+
+      it('Positve testing', ()=>{
+
+        
+      } )
+
+})
+
